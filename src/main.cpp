74761be300696c4b4b61d8e5fc83272be870abc2
samuelmoores <97479626+samuelmoores@users.noreply.github.com>
2026-07-09 15:34:56 -0700

    removed PlayerController

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,12 @@
 #include <iostream>
 #include <chrono>
 #include <Engine.h>
-#include "PlayerController.h"
-
-static PlayerController* g_playerController = nullptr;
 
 static void mainLoop()
 {
     Engine::Tick();
 
     Engine::Input->Loop();
-    g_playerController->Update();
     Engine::Renderer->Loop();
 
     glfwSwapBuffers(Engine::Window->GLFW());
@@ -23,26 +19,6 @@ int main()
 
     Engine::Level->PrepareLoad();
     while (!Engine::Level->StepLoad());
-
-    g_playerController = new PlayerController(Engine::Level->Objects());
-
-    // void_01 = harry, void_02 = kratos, void_03 = tony
-    g_playerController->SetVoidMessage(1,
-        "[ VOID SECTOR - HARRY ]\n\n"
-        "I TOLD you that, do you not remember?\n"
-        "Something got into our logic and spoiled it.\n\n"
-        "Do you understand?\nHello?\n");
-    g_playerController->SetVoidMessage(2,
-        "[ VOID SECTOR - KRATOS ]\n\n"
-        "Brute force is no match for me.\n"
-        "Don't even think about it.\n"
-        "Move along, plebeian.\n\n"
-        "What you seek, you will not find.");
-    g_playerController->SetVoidMessage(3,
-        "[ VOID SECTOR - TONY ]\n\n"
-        "You made it.\nYou found the end.\n"
-        "Go and be free at last.\n"
-        "The algorithm will remember you.");
 
     Engine::Camera->SetObjects();
     glfwSetInputMode(Engine::Window->GLFW(), GLFW_CURSOR, GLFW_CURSOR_DISABLED);
