b9e9e295f06c61c6881260958667c50948f40472
samuelmoores <97479626+samuelmoores@users.noreply.github.com>
2026-07-09 18:06:12 -0700

    input stubbed

--- include/Input.h
+++ include/Input.h
@@ -1,51 +1,8 @@
 #pragma once
-#include <unordered_map>
-#include <glm/glm.hpp>
-
-enum class Action {
-	MoveForward,
-	MoveBack,
-	MoveLeft,
-	MoveRight,
-	Escape,
-	Interact
-};
-
-struct ActionState {
-	bool isDown       = false;
-	bool justPressed  = false;
-	bool justReleased = false;
-};
-
-struct GLFWwindow;
 
 class Input {
 public:
 	Input();
 	void Loop();
-
-	bool      IsDown(Action a) const;
-	bool      JustPressed(Action a) const;
-	bool      JustReleased(Action a) const;
-	glm::vec2 MoveInput() const;
-	void      ResetMouseLook();
-
-private:
-	void UpdateActionStates();
-
-	static void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
-	static void CharCallback(GLFWwindow* window, unsigned int codepoint);
-	static void CursorPosCallback(GLFWwindow* window, double xpos, double ypos);
-	static void CursorEnterCallback(GLFWwindow* window, int entered);
-	static void MouseButtonCallback(GLFWwindow* window, int button, int action, int mods);
-	static void ScrollCallback(GLFWwindow* window, double xoffset, double yoffset);
-	static void FramebufferSizeCallback(GLFWwindow* window, int width, int height);
-
-	std::unordered_map<int, Action>         m_bindings;
-	std::unordered_map<Action, ActionState> m_actions;
-
-	glm::vec2 m_mouseLast      = glm::vec2(0);
-	glm::vec2 m_mouseCurr      = glm::vec2(0);
-	bool      m_windowActive   = true;
-	bool      m_mouseHasMoved  = false;
+	void ResetMouseLook();
 };
