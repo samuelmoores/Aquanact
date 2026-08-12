69e6cbe2dbb718d7a81a2d48f4dc47ceb8dbf810
samuelmoores <97479626+samuelmoores@users.noreply.github.com>
2026-08-12 13:08:43 -0700

    added PhysicsCollider in prep for new PhysicsWorld singleton

---
+++ include/Engine/Core/PhysicsCollider.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "Engine/Core/Entity.h"
+
+#include <cstddef>
+#include <glm/glm.hpp>
+
+struct PhysicsCollider
+{
+	// The world does not own the entity; this is used to identify collision results.
+	Entity* owner = nullptr;
+
+	PhysicsColliderShape shape = PhysicsColliderShape::Box;
+
+	glm::vec3 minBounds{ 0.0f };
+	glm::vec3 maxBounds{ 0.0f };
+
+	bool enabled = true;
+};
+
+using ColliderHandle = std::size_t;
